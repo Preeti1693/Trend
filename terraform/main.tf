5e555ef662b6f81715cc654788897867042ff87b
root <root@ip-172-31-20-139.ec2.internal>
2026-02-05 20:40:15 +0000

    updated main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -51,6 +51,9 @@ module "eks" {
   cluster_name    = "trend-eks"
   cluster_version = "1.29"
 
+  cluster_endpoint_public_access = true
+  cluster_endpoint_private_access = false
+
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.public_subnets
 
